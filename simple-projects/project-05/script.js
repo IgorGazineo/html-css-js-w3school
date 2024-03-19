7fceb38f67ed491d214acbac886fbc42714b5775
Igor Gazineo <gaumgazineo@hotmail.com>
2024-03-19 14:25:55 -0300

    closing the menu when a link in the menu is clicked

--- simple-projects/project-05/script.js
+++ simple-projects/project-05/script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector(".menu-hamburger");
 const closeMenuBtn = document.querySelector(".close-btn button");
 const navElements = document.querySelector("header nav ul");
+const navLinks = document.querySelectorAll("nav a");
 
 menuBtn.addEventListener("click", () => {
   navElements.classList.add("active");
@@ -8,4 +9,12 @@ menuBtn.addEventListener("click", () => {
 
 closeMenuBtn.addEventListener("click", () => {
   navElements.classList.remove("active");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (navElements.classList.value === "active") {
+      navElements.classList.remove("active");
+    }
+  });
 });
